f53b82c6ca51a843a4bc77bb7431d78cd1433d92
KrisKy02 <kristel.herrera@ucr.ac.cr>
2023-12-07 17:38:33 -0600

    Mejora de revision.py

--- revision.py
+++ revision.py
@@ -1,54 +1,67 @@
-from proceso import *
+"""
+Análisis de Datos Ambientales Usando `proceso.py`.
+
+Este script utiliza el módulo `proceso.py` para realizar una serie de análisis
+estadísticos y visualizaciones de datos ambientales. El script demuestra cómo
+utilizar cada una de las funciones proporcionadas por el módulo, incluyendo
+la generación de muestras, análisis de proceso, visualizaciones en 2D y 3D,
+y cálculos de autocorrelación, autocovarianza, media temporal y ergodicidad.
+
+El script se estructura de la siguiente manera:
+- Carga y preparación de los datos ambientales.
+- Ejemplo de uso de cada función del módulo `proceso`.
+- Análisis estadístico de los datos utilizando las funciones del módulo.
+"""
 import os
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
-from scipy import stats
-from numpy.polynomial.polynomial import Polynomial
 import scipy.stats as st
 import seaborn as sns
 import matplotlib.pyplot as plt
 from datetime import datetime
 from mpl_toolkits.mplot3d import Axes3D
+from numpy.polynomial.polynomial import Polynomial
+from proceso import *
 
-# Carga el DataFrame
+# ----- Carga y Preparación de Datos -----
 data = pd.read_csv('seoul.csv')
-
-# Ejemplo de uso de la función muestra:
-muestra_ejemplo = muestra(data, 'o3', 113, 2020031400, 2020031723)
-print(muestra_ejemplo.head())
-
-# Ejemplo de uso de la función proceso:
-proceso_ejemplo = proceso(data, 'o3', 113, 20200314, 20200317)
-# Imprimir los días para los que se han extraído funciones muestra.
-print(list(proceso_ejemplo.keys()))
-lista_fechas = [['2020031400', '2020031423'], ['2020031500', '2020031523']]
-grafica2d(data, 'o3', 113, lista_fechas, 'graficas2d')
-grafica3d(data, 'o3', 113, '2020031400', '2020031723')
-# Ejemplo de uso de la función:
-distribucion(data, 'o3', 113)
-
-# Prepara el DataFrame
 data['dt'] = pd.to_datetime(data['dt'], format='%Y%m%d%H')
 data.set_index('dt', inplace=True)
 
-# Ahora puedes llamar a las funciones
+# ----- Ejemplos de Uso de Funciones -----
+# Función muestra
+muestra_ejemplo = muestra(data, 'o3', 113, 2020031400, 2020031723)
+print("Ejemplo de función muestra:\n", muestra_ejemplo.head())
+
+# Función proceso
+proceso_ejemplo = proceso(data, 'o3', 113, 20200314, 20200317)
+print("Claves del proceso ejemplo:", list(proceso_ejemplo.keys()))
+
+# Función grafica2d
+lista_fechas = [['2020031400', '2020031423'], ['2020031500', '2020031523']]
+grafica2d(data, 'o3', 113, lista_fechas, 'graficas2d')
+
+# Función grafica3d
+grafica3d(data, 'o3', 113, '2020031400', '2020031723')
+
+# Función distribucion
+distribucion(data, 'o3', 113)
+
+# ----- Análisis Estadístico -----
+# Autocorrelación y Autocovarianza
 autocorr_value = autocorrelacion(data, 'o3', 9, 17)
 print(f"Autocorrelación entre las horas 9 y 17: {autocorr_value}")
 
 autocovarianza_val = autocovarianza(data, 'o3', 9, 17)
 print(f"Autocovarianza entre las 9 y las 17: {autocovarianza_val}")
 
-
+# Media Temporal y Ergodicidad
 promedio = prom_temporal(data, 'o3', inicio='2020031400', fin='2020031723')
 print(f"El promedio temporal es: {promedio}")
+
 resultado_ergodicidad = ergodicidad(data, 'o3')
 print(f"La secuencia aleatoria M(t) es ergódica: {resultado_ergodicidad}")
-# Prepara la muestra para wss
-muestra_ejemplo['dt'] = pd.to_datetime(muestra_ejemplo['dt'],
-                                       format='%Y%m%d%H')
-muestra_ejemplo.set_index('dt', inplace=True)
 
-# Llama a la función wss para la muestra
-resultado_wss_muestra = wss(muestra_ejemplo, 'o3', 'dt')
+# Estacionariedad en Sentido Amplio
+resultado_wss_muestra = wss(data, 'o3', 'dt')
 print(f"La muestra es estacionaria en sentido amplio: {resultado_wss_muestra}")
